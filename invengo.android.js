bf4f681c8aa938d56503ee7f0d4d0993e72a5a2d
Joe Pleso <joseph.pleso@philips.com>
2017-06-24 13:55:12 -0400

    fix(*): add js

--- invengo.android.js
+++ invengo.android.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var Invengo = (function () {
     function Invengo() {
         this.DEFULT_BANK = com.atid.lib.dev.rfid.type.BankType.EPC;
